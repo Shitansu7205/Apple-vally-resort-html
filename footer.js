5ad6ab9d31a2d919806836044af091e2a9ce8cd6
Shitansu7205 <shitansukumargochhayat@gmail.com>
2026-05-29 02:39:13 +0530

    Working on Static pages

---
+++ footer.js
@@ -0,0 +1,174 @@
+document.write(`
+        <!--======  Start Footer  ======-->
+    <footer
+      class="main-footer bg_cover"
+      data-src="assets/images/apple-valley-imgs/footer-bg.webp"
+    >
+      <!--=== Footer Top ===-->
+      <div class="container">
+        <div class="footer-top-area" data-aos="fade-up" data-aos-duration="800">
+          <div class="row">
+            <div class="col-lg-4 col-md-6">
+              <div class="luxivo-info-box style-one">
+                <div class="icon">
+                  <i class="fal fa-phone-volume"></i>
+                </div>
+                <div class="content">
+                  <span>Call us Anytime</span>
+                  <h5><a href="tel:+919218660001">+91 92186-60001</a></h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="luxivo-info-box style-one">
+                <div class="icon">
+                  <i class="fal fa-envelope"></i>
+                </div>
+                <div class="content">
+                  <span>Send us E-Mail</span>
+                  <h5>
+                    <a href="mailto:reservations@avresorts.in"></a>
+                      reservations@avresorts.in</a
+                    >
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="luxivo-info-box style-one">
+                <div class="icon">
+                  <i class="fal fa-map-marker-alt"></i>
+                </div>
+                <div class="content">
+                  <span>Address</span>
+                  <h5>Mohal, Himachal Pradesh-175126</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--=== Footer Widget Area ===-->
+      <div class="footer-widget-area pt-80 pb-40">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-xl-4 col-md-6 col-sm-12 order-1">
+              <!--=== Footer Widget ===-->
+              <div
+                class="footer-widget footer-about-widget mb-40"
+                data-aos="fade-up"
+                data-aos-duration="800"
+              >
+                <div class="widget-content">
+                  <div class="footer-logo mb-30">
+                    <img
+                      src="assets/images/apple-valley-imgs/logon.png"
+                      alt="Logo White"
+                      style="width: 200px; height: auto"
+                    />
+                  </div>
+                  <p>
+                    Experience peaceful riverside stays, scenic mountain views,
+                    and warm hospitality at Apple Valley Resort, designed for
+                    comfort, relaxation, and memorable vacations.
+                  </p>
+                  <div class="newsletter-box">
+                    <form>
+                      <div class="form-group">
+                        <input
+                          type="email"
+                          placeholder="Enter your email"
+                          class="form_control"
+                          required
+                        />
+                        <button class="theme-btn style-one">
+                          SUBSCRIBE <i class="far fa-paper-plane"></i>
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-5 col-sm-12 order-3 order-xl-2">
+              <div class="row">
+                <div class="col-md-6">
+                  <!--=== Footer Widget ===-->
+                  <div
+                    class="footer-widget footer-nav-widget mb-40"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                  >
+                    <div class="widget-content">
+                      <h4 class="widget-title">Quick Links</h4>
+                      <ul class="widget-nav">
+                        <li><a href="index.html#about">About Hotel</a></li>
+                        <li><a href="index.html#rooms">Rooms & Suites</a></li>
+                        <li><a href="index.html#services">Services</a></li>
+                        <li><a href="gallery.html">Gallery</a></li>
+                        <li><a href="index.html#contact">Contact</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <!--=== Footer Widget ===-->
+                  <div
+                    class="footer-widget footer-nav-widget mb-40"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                  >
+                    <div class="widget-content">
+                      <h4 class="widget-title">Services</h4>
+                      <ul class="widget-nav">
+                        <li><a href="#">Non-Smoking Rooms </a></li>
+                        <li><a href="#">Comfortable Beds </a></li>
+
+                        <li><a href="#">Welcome Drink </a></li>
+                        <li><a href="#">Riverside Location </a></li>
+                        <li><a href="#">Delicious Dining </a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-md-6 col-sm-12 order-2 order-xl-3">
+              <!--=== Footer Widget ===-->
+              <div
+                class="footer-widget footer-post-widget mb-40"
+                data-aos="fade-up"
+                data-aos-duration="1400"
+              >
+                <div class="widget-content">
+                  <h4 class="widget-title">Recent Post</h4>
+                  <div class="recent-post-list">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13617.48318092698!2d77.128479!3d31.91389!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3904f719cd916fbd%3A0x623910b74584aa4c!2sApple%20Valley%20Resort!5e1!3m2!1sen!2sin!4v1780001188571!5m2!1sen!2sin" width="200" height="200" style="border-radius: 5px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--=== Copyright Area ===-->
+      <div class="copyright-area">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-12">
+              <!--=== Copyright Text ===-->
+              <div class="copyright-text text-center">
+                <p>&copy; All Copyright 2026 by Apple Valley Resort || Powered by <a href="https://bookingjini.com" target="_blank" style="color: #007bff;">Bookingjini</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!--====== End Footer  ======-->
+    <!--====== Back To Top  ======-->
+    <div class="back-to-top"><i class="far fa-angle-up"></i></div>
+        <script src="https://jiniassist.bookingjini.com/embed.js?company_id=1643"></script>
+
+    
+    `);
---
+++ header.js
@@ -0,0 +1,110 @@
+document.write(`
+    <!--====== Start Preloader ======-->
+    <div class="preloader">
+      <div class="loading-wrapper">
+        <div class="loading"></div>
+        <div id="loading-icon">
+          <img src="assets/images/loader.png" alt="loader" />
+        </div>
+      </div>
+    </div>
+    <!--====== End Preloader ======-->
+    <!--====== Start Overlay ======-->
+    <div class="offcanvas__overlay"></div>
+    <!--=== Start  Header Area  ===-->
+    <header class="header-area header-one transparent-header">
+      <!--====  Header Navigation  ===-->
+      <div class="header-navigation">
+        <div class="container-fluid">
+          <!--====  Primary Menu  ===-->
+          <div class="primary-menu">
+            <!--====  Site Branding  ===-->
+            <div class="site-branding">
+              <a href="index.html" class="brand-logo"
+                ><img
+                  src="assets/images/apple-valley-imgs/logon.png"
+                  alt="Brand Logo"
+                  style="width: 140px"
+              /></a>
+            </div>
+            <!--=== Main Menu ===-->
+            <div class="theme-nav-menu">
+              <!--=== Menu Top ===-->
+              <div class="theme-menu-top d-block d-xl-none">
+                <div class="site-branding">
+                  <a href="index.html" class="brand-logo"
+                    ><img
+                      src="assets/images/apple-valley-imgs/logon.png"
+                      alt="Brand Logo"
+                      style="width: 80px"
+                  /></a>
+                </div>
+              </div>
+              <!--=== Main Menu ===-->
+              <nav class="main-menu">
+                <ul>
+                  <li class="menu-item ">
+                    <a href="index.html">Home</a>
+                  </li>
+                  <li class="menu-item ">
+                    <a href="index.html#about">About Us</a>
+                  </li>
+                  <li class="menu-item ">
+                    <a href="index.html#rooms">Accommodation</a>
+                  </li>
+                  <li class="menu-item ">
+                    <a href="gallery.html"> Gallery</a>
+                  </li>
+                  <li class="menu-item ">
+                    <a href="index.html#services">Services</a>
+                  </li>
+                  <li class="menu-item ">
+                    <a href="index.html#contact">Contact Us</a>
+                  </li>
+                </ul>
+              </nav>
+              <!--=== Nav Button ===-->
+              <div class="theme-nav-button mt-20 d-block d-md-none">
+                <a href="https://applevalleyresorts.bookingjini.in/ " class="theme-btn style-one"
+                  >Book Now<i class="far fa-arrow-right"></i
+                ></a>
+              </div>
+              <!--===  Menu Bottom ===-->
+              <div class="theme-menu-bottom mt-50 d-block d-xl-none">
+                <h5>Follow Us</h5>
+                <ul class="social-link">
+                  <li>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!--=== Header Nav Right ===-->
+            <div class="nav-right-item">
+              <div class="nav-button d-none d-md-block">
+                <a href="https://applevalleyresorts.bookingjini.in/" class="theme-btn style-one"
+                  >Book Your stay<i class="far fa-arrow-right"></i
+                ></a>
+              </div>
+              <div class="navbar-toggler">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!--=== End  Header Area  ===-->
+    
+    `)
